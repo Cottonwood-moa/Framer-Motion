8a5f8b8cd4cf37e4d5861cc9e13c724ae07b17eb
Cottonwood <blog_geon@naver.com>
2022-03-29 14:30:42 +0900

    no message

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import styled from "styled-components";
 import { motion } from "framer-motion";
-import { useRef } from "react";
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -9,16 +8,7 @@ const Wrapper = styled.div`
   justify-content: center;
   align-items: center;
 `;
-const BiggerBox = styled.div`
-  width: 600px;
-  height: 600px;
-  background-color: rgba(255, 255, 255, 0.2);
-  border-radius: 40px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  overflow: hidden;
-`;
+
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
@@ -33,30 +23,12 @@ const Box = styled(motion.div)`
 const boxVariants = {
   hover: { scale: 1.5, rotateZ: 90 },
   click: { scale: 1, borderRadius: "100px" },
-  drag: {
-    backgroundColor: "rgb(0, 168, 255)",
-    transition: {
-      duration: 1,
-    },
-  },
 };
 
 function App() {
-  const biggerBoxRef = useRef<HTMLDivElement>(null);
   return (
     <Wrapper>
-      <BiggerBox ref={biggerBoxRef}>
-        <Box
-          variants={boxVariants}
-          drag
-          dragConstraints={biggerBoxRef} // drag가능한 범위 요소 지정
-          dragSnapToOrigin // drag후 원래 있던 자리로
-          dragElastic={0.0001} // 당기는 힘. 0~1사이 값, default:0.5
-          whileHover="hover"
-          whileTap="click"
-          whileDrag="drag"
-        ></Box>
-      </BiggerBox>
+      <Box variants={boxVariants}></Box>
     </Wrapper>
   );
 }

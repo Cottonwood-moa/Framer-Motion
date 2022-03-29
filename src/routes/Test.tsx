6dab9f488de79021d31b61c78d7807dd75c0d443
Cottonwood <blog_geon@naver.com>
2022-03-30 03:43:33 +0900

    no message

--- src/routes/Test.tsx
+++ src/routes/Test.tsx
@@ -1,15 +1,25 @@
 import styled from "styled-components";
-import { AnimatePresence } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { Link } from "react-router-dom";
-const Container = styled.div`
+const Container = styled(motion.div)`
   width: 100vw;
   height: 100vh;
   background-color: red;
 `;
+const containerVars = {
+  start: {},
+  end: {},
+  exit: {},
+};
 function Test() {
   return (
     <AnimatePresence>
-      <Container>
+      <Container
+        variants={containerVars}
+        initial="start"
+        animate="end"
+        exit="exit"
+      >
         <Link to={`/`}>Go to Home</Link>
       </Container>
     </AnimatePresence>

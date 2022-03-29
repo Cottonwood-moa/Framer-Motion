7e9bd9608de62169a9161cc05ddf01f0cfb3ca76
Cottonwood <blog_geon@naver.com>
2022-03-30 03:43:30 +0900

    no message

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -2,17 +2,27 @@ import { motion } from "framer-motion";
 import styled from "styled-components";
 import { AnimatePresence } from "framer-motion";
 import { Link } from "react-router-dom";
-const Container = styled.div`
+const Container = styled(motion.div)`
   width: 100vw;
   height: 100vh;
-  background-color: royalblue;
 `;
 const Card = styled(motion.div)``;
+
+const containerVars = {
+  start: {},
+  end: {},
+  exit: {},
+};
 
 function Home() {
   return (
     <AnimatePresence>
-      <Container>
+      <Container
+        variants={containerVars}
+        initial="start"
+        animate="end"
+        exit="exit"
+      >
         <Link to={`/test`}>Go to Test</Link>
       </Container>
       ;

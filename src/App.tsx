483ac3f7aedd8991d1eb5df1bd80be82cb1ebaae
Cottonwood <blog_geon@naver.com>
2022-03-29 14:38:58 +0900

    no message

--- src/App.tsx
+++ src/App.tsx
@@ -12,23 +12,45 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: white;
+  background-color: rgba(0, 0, 0, 0.1);
   border-radius: 15px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
   font-weight: bold;
 `;
+const Circle = styled(motion.div)`
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  background-color: white;
+  place-self: center;
+`;
 const boxVariants = {
-  hover: { scale: 1.5, rotateZ: 90 },
-  click: { scale: 1, borderRadius: "100px" },
+  start: {
+    opacity: 0,
+  },
+  end: {
+    opacity: 1,
+    transition: {
+      delayChildren: 0.3,
+      staggerChildren: 0.2,
+    },
+  },
 };
-
+const circleVariants = {
+  start: { opacity: 0, y: 10 },
+  end: { opacity: 1, y: 0 },
+};
 function App() {
   return (
     <Wrapper>
-      <Box variants={boxVariants}></Box>
+      <Box variants={boxVariants} initial={`start`} animate={`end`}>
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+      </Box>
     </Wrapper>
   );
 }

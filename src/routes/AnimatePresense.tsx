1ef093e1ecf596a5a7e2f90d228fcff3b27f606c
Cottonwood <blog_geon@naver.com>
2022-03-31 02:50:23 +0900

    no message

--- src/routes/AnimatePresense.tsx
+++ src/routes/AnimatePresense.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import { motion, AnimatePresence } from "framer-motion";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 const Wrapper = styled(motion.div)`
   height: 100vh;
@@ -27,6 +27,19 @@ const Box = styled(motion.div)`
   align-items: center;
   font-weight: bold;
 `;
+const Toggle = styled.div`
+  position: absolute;
+  width: 100px;
+  height: 60px;
+  background-color: rgba(0, 0, 0, 0.5);
+  font-weight: bold;
+  border-radius: 20px;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+`;
 const boxVariants = {
   start: {
     scale: 0,
@@ -45,20 +58,23 @@ const boxVariants = {
 };
 function AnimatePresense() {
   const [isToggled, setIsToggled] = useState(false);
+  const wrapRef = useRef<HTMLDivElement>(null);
   return (
-    <Wrapper>
+    <Wrapper ref={wrapRef}>
       <AnimatePresence>
         {isToggled ? (
           <Box
             drag
+            dragConstraints={wrapRef}
             variants={boxVariants}
+            dragElastic={0.0001}
             initial={`start`}
             animate={`end`}
             exit={`hide`}
           />
         ) : null}
       </AnimatePresence>
-      <button onClick={() => setIsToggled((prev) => !prev)}>버튼</button>
+      <Toggle onClick={() => setIsToggled((prev) => !prev)}>버튼</Toggle>
     </Wrapper>
   );
 }
